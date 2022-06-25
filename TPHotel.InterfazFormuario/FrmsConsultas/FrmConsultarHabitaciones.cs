33f4d908a3362e9fcc11877818e1263dd2a238e3
DarioAlen <102334418+DarioAlen@users.noreply.github.com>
2022-06-25 10:22:25 -0300

    Agregado líneas 38 a 51 y 59 a 64

--- TPHotel.InterfazFormuario/FrmsConsultas/FrmConsultarHabitaciones.cs
+++ TPHotel.InterfazFormuario/FrmsConsultas/FrmConsultarHabitaciones.cs
@@ -35,12 +35,33 @@ namespace TPHotel.InterfazFormuario
             if (listaHabitaciones.Count == 0)
             {
                 MessageBox.Show("El hotel no posee habitaciones");
+                //Agregado el 25/06/22
+                 _lstHabitaciones.DataSource = null;
+                _lstHabitaciones.Items.Clear();
+
+                _txtIdHabitacion.Text = string.Empty;
+                _txtHabNmbr.Text = string.Empty;
+                _txtEstrellas.Text = string.Empty;
+
+                _txtIdHabitacion.Enabled = false;
+                _txtHabNmbr.Enabled = false;
+                _txtEstrellas.Enabled = false;
+
+                button1.Enabled = false;
+                //
             }
 
             else
             {
                 _lstHabitaciones.DataSource = null;
                 _lstHabitaciones.DataSource = listaHabitaciones;
+                
+                //Agregado el 25/06/22
+                _txtIdHabitacion.Enabled = true;
+                _txtHabNmbr.Enabled = true;
+                _txtEstrellas.Enabled = true;
+                button1.Enabled = true;
+                //
             }
 
 
